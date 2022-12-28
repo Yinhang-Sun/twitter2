9f9122a270823b78dd353c74ccd0ffb799978e6c
yinhang_sun <sunyinhang@gmail.com>
2022-12-27 23:41:16 -0800

    implement 32-redis-as-cache

--- testing/testcases.py
+++ testing/testcases.py
@@ -7,11 +7,12 @@ from likes.models import Like
 from newsfeeds.models import NewsFeed
 from rest_framework.test import APIClient
 from tweets.models import Tweet
-
+from utils.redis_client import RedisClient
 
 class TestCase(DjangoTestCase):
 
     def clear_cache(self):
+        RedisClient.clear()
         caches['testing'].clear()
 
     @property
@@ -20,7 +21,6 @@ class TestCase(DjangoTestCase):
             return self._anonymous_client
         self._anonymous_client = APIClient()
         return self._anonymous_client
-
     def create_user(self, username, email=None, password=None):
         if email is None:
             email = '{}@jiuzhang.com'.format(username)
@@ -29,20 +29,16 @@ class TestCase(DjangoTestCase):
         # 不能写成 User.objects.create()
         # 因为 password 需要被加密, username 和 email 需要进行一些 normalize 处理
         return User.objects.create_user(username, email, password)
-
     def create_tweet(self, user, content=None):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
-
     def create_newsfeed(self, user, tweet):
         return NewsFeed.objects.create(user=user, tweet=tweet)
-
     def create_comment(self, user, tweet, content=None):
         if content is None:
             content = 'default comment content'
         return Comment.objects.create(user=user, tweet=tweet, content=content)
-
     def create_like(self, user, target):
         instance, _ = Like.objects.get_or_create(
             content_type=ContentType.objects.get_for_model(target.__class__),
@@ -50,7 +46,6 @@ class TestCase(DjangoTestCase):
             user=user,
         )
         return instance
-
     def create_user_and_client(self, *args, **kwargs):
         user = self.create_user(*args, **kwargs)
         client = APIClient()
--- tweets/tests.py
+++ tweets/tests.py
@@ -2,22 +2,30 @@ from datetime import timedelta
 from testing.testcases import TestCase
 from tweets.constants import TweetPhotoStatus
 from tweets.models import TweetPhoto
+from utils.redis_client import RedisClient
+from utils.redis_serializers import DjangoModelSerializer
 from utils.time_helpers import utc_now
 
 
 class TweetTests(TestCase):
+
     def setUp(self):
+        self.clear_cache()
         self.linghu = self.create_user('linghu')
         self.tweet = self.create_tweet(self.linghu, content='Jiuzhang Dafa Hao')
+
     def test_hours_to_now(self):
         self.tweet.created_at = utc_now() - timedelta(hours=10)
         self.tweet.save()
         self.assertEqual(self.tweet.hours_to_now, 10)
+
     def test_like_set(self):
         self.create_like(self.linghu, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 1)
+
         self.create_like(self.linghu, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 1)
+
         dongxie = self.create_user('dongxie')
         self.create_like(dongxie, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
@@ -31,3 +39,15 @@ class TweetTests(TestCase):
         self.assertEqual(photo.user, self.linghu)
         self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
         self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
+
+    def test_cache_tweet_in_redis(self):
+        tweet = self.create_tweet(self.linghu)
+        conn = RedisClient.get_connection()
+        serialized_data = DjangoModelSerializer.serialize(tweet)
+        conn.set(f'tweet:{tweet.id}', serialized_data)
+        data = conn.get(f'tweet:not_exists')
+        self.assertEqual(data, None)
+
+        data = conn.get(f'tweet:{tweet.id}')
+        cached_tweet = DjangoModelSerializer.deserialize(data)
+        self.assertEqual(tweet, cached_tweet)
