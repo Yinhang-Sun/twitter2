39c53406e6a727fdf581ee033a0ba3bf0e6870e0
yinhang_sun <sunyinhang@gmail.com>
2023-01-03 12:21:45 -0800

    implement 35-cache-with-limited-size

--- testing/testcases.py
+++ testing/testcases.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.cache import caches
 from django.test import TestCase as DjangoTestCase
+from friendships.models import Friendship
 from likes.models import Like
 from newsfeeds.models import NewsFeed
 from rest_framework.test import APIClient
 from tweets.models import Tweet
 from utils.redis_client import RedisClient
+
 
 class TestCase(DjangoTestCase):
 
@@ -21,6 +23,7 @@ class TestCase(DjangoTestCase):
             return self._anonymous_client
         self._anonymous_client = APIClient()
         return self._anonymous_client
+
     def create_user(self, username, email=None, password=None):
         if email is None:
             email = '{}@jiuzhang.com'.format(username)
@@ -29,16 +32,23 @@ class TestCase(DjangoTestCase):
         # 不能写成 User.objects.create()
         # 因为 password 需要被加密, username 和 email 需要进行一些 normalize 处理
         return User.objects.create_user(username, email, password)
+
+    def create_friendship(self, from_user, to_user):
+        return Friendship.objects.create(from_user=from_user, to_user=to_user)
+
     def create_tweet(self, user, content=None):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content=content)
+
     def create_newsfeed(self, user, tweet):
         return NewsFeed.objects.create(user=user, tweet=tweet)
+
     def create_comment(self, user, tweet, content=None):
         if content is None:
             content = 'default comment content'
         return Comment.objects.create(user=user, tweet=tweet, content=content)
+
     def create_like(self, user, target):
         instance, _ = Like.objects.get_or_create(
             content_type=ContentType.objects.get_for_model(target.__class__),
@@ -46,6 +56,7 @@ class TestCase(DjangoTestCase):
             user=user,
         )
         return instance
+
     def create_user_and_client(self, *args, **kwargs):
         user = self.create_user(*args, **kwargs)
         client = APIClient()
